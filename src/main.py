fe7d0ea046554d3e10dba6402e670e77fea520ad
Kirill Zhuravlev <a24317@yandex.ru>
2024-06-22 18:54:29 +0300

    fix small issues

--- src/main.py
+++ src/main.py
@@ -7,16 +7,13 @@
 Пример использования:
     python main.py
 """
-
+import argparse
 import cv2 as cv
 from detection import PeopleDetector
 from drawing_bb import draw_predictions
 import os
 
-SOURCE_PATH = 'data/crowd.mp4'
-THRESHOLD = 0.9
-
-def main(SOURCE_PATH: str = SOURCE_PATH, SAVE_PATH: str = "output/sample_output.avi") -> None:
+def process_video(SOURCE_PATH: str , SAVE_PATH: str, threshold: float) -> None:
     """
     Обрабатывает видеофайл, выполняет детекцию людей на каждом кадре и сохраняет обработанное видео
 
@@ -58,7 +55,7 @@ def main(SOURCE_PATH: str = SOURCE_PATH, SAVE_PATH: str = "output/sample_output.
         preds = detector.detect(frame)
 
         # отрисовываем на кадре предсы
-        frame = draw_predictions(frame, preds, threshold=THRESHOLD)
+        frame = draw_predictions(frame, preds, threshold=threshold)
 
         # записываем результат
         out.write(frame)
@@ -72,7 +69,24 @@ def main(SOURCE_PATH: str = SOURCE_PATH, SAVE_PATH: str = "output/sample_output.
 
     # проверка был ли файл сохранен
     if not os.path.exists(SAVE_PATH):
-        print("Ошибка. Видео не сохранилдось")
+        print("Ошибка. Видео не сохранилось")
+
+def main():
+    parser = argparse.ArgumentParser(description="Детектирует людей и рисует боксы.")
+    parser.add_argument("--input_folder", type=str, default="data", help="Папка с исходным видео.")
+    parser.add_argument("--output_folder", type=str, default="output", help="Папка для сохранения обработанного видео.")
+    parser.add_argument("--threshold", type=float, default=0.8, help="Трешхолд увернности модели в классе")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_folder):
+        os.makedirs(args.output_folder)
+
+    for filename in os.listdir(args.input_folder):
+        if filename.endswith(".mp4"):
+            input_path = os.path.join(args.input_folder, filename)
+            output_path = os.path.join(args.output_folder, f"output_{filename}")
+            process_video(input_path, output_path, args.threshold)
 
 if __name__ == "__main__":
     main()
